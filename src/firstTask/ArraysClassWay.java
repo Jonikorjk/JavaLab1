d1af4f4102bbe0e754214c508b4ea6d234b7023d
Jon1k <zenaserdykov2004@gmail.com>
2022-10-26 17:06:08 +0300

    all

--- src/firstTask/ArraysClassWay.java
+++ src/firstTask/ArraysClassWay.java
@@ -1,2 +1,67 @@
-package firstTask;public class ArraysClassWay {
+package firstTask;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ *  The class which solve task using java.util.Arrays
+ */
+public class ArraysClassWay {
+
+    final public static String fillSymbol = "$";
+
+    /**
+     * <p>fill1DArray()</p>
+     * Creates an array of ints enters a random value from 5 to 21 (inclusive)
+     * Used in conjunction with the java.util.Arrays class
+     * Function parameters:
+     * @param length the length of the returned array
+     * @return an array of ints filled with random numbers from the interval [4,33]
+     */
+    public static int[] fill1DArray(int length) {
+        Random rand = new Random();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = rand.nextInt(4, 34);
+        }
+        return arr;
+    }
+
+    /**
+     * <p>getMax()</p>
+     * Function to get the maximum element in a row
+     * Used in conjunction with the java.util.Arrays class
+     * Function parameters:
+     * @param arr Reference to a two-dimensional array of ints
+     * @param i an int variable that specifies the row to be searched
+     * @return The maximum element in the column
+     */
+    public static int getMax(int[][] arr, int i) {
+        Integer[] row = new Integer[arr[0].length];
+        Arrays.setAll(row, j -> arr[i][j]);
+        Arrays.sort(row, Collections.reverseOrder());
+        return row[0];
+    }
+
+    public void start(int rows, int cols) {
+        System.out.println("class Arrays Way");
+        int[][] s_intArr = new int[rows][];
+        int[] arr_of_max = new int[rows];
+        String[] s_strArr = new String[rows];
+
+        Arrays.setAll(s_intArr, i -> fill1DArray(cols));
+        System.out.println("2D Array");
+        System.out.println((Arrays.deepToString(s_intArr)));
+
+        System.out.println("Array of max");
+        Arrays.setAll(arr_of_max, i -> getMax(s_intArr, i));
+        System.out.println(Arrays.toString(arr_of_max));
+
+        Arrays.setAll(s_strArr, i -> fillSymbol.repeat(arr_of_max[i]));
+        Arrays.sort(s_strArr, (s1, s2) -> -Integer.compare(s2.length(), s1.length()));
+
+        System.out.println("Result");
+        System.out.println(Arrays.toString(s_strArr));
+    }
 }
--- src/firstTask/StandartWay.java
+++ src/firstTask/StandartWay.java
@@ -1,2 +1,156 @@
-package firstTask;public class StandartWay {
+package firstTask;
+
+import java.util.Random;
+
+/**
+ * The class that solve task standart way
+ */
+public class StandartWay {
+    final public static String fillSymbol = "$";
+
+    /**
+     * <p>sortStringArray()</p>
+     * Sorting function, uses bubble sort algorithm
+     * Function parameters:
+     * @param strArr Reference to an array of strings
+     */
+    public static void sortStringArray(String[] strArr) {
+        String buffer = "";
+        boolean mustSort = false;
+
+        do {
+            mustSort = false;
+            for (int i = 0; i < strArr.length - 1; i++) {
+                if (strArr[i].length() > strArr[i + 1].length()) {
+                    buffer = strArr[i];
+                    strArr[i] = strArr[i + 1];
+                    strArr[i + 1] = buffer;
+                    mustSort = true;
+                    break;
+                }
+            }
+        } while (mustSort);
+    }
+
+    /**
+     * <p>getMaxOfRow()</p>
+     * Function to get the maximum element in a row
+     * Function parameters:
+     * @param arr Reference to a two-dimensional array of ints
+     * @param row is an int variable that specifies the row to be searched
+     * @return The maximum element in the row
+     */
+    public static int getMaxOfRow(int[][] arr, int row)
+    {
+        int max = arr[row][0];
+        for (int i = 0; i < arr[0].length; i++) {
+            if (max < arr[row][i]) {
+                max = arr[row][i];
+            }
+        }
+//        System.out.println(max);
+        return max;
+    }
+
+    /**
+     * <p>fillStringArray()</p>
+     * Function to populate an array of strings
+     * Function parameters:
+     * @param stringArr Reference to a one-dimensional array of strings
+     * @param intArray Reference to a two-dimensional array of ints
+     */
+    public static void fillStringArray(String[] stringArr, int[][] intArray)
+    {
+        for (int i = 0; i < stringArr.length; i++) {
+            stringArr[i] = "";
+        }
+        for (int i = 0; i < stringArr.length; i++) {
+            for (int j = 0; j < getMaxOfRow(intArray,i); j++) {
+                stringArr[i] += fillSymbol;
+            }
+        }
+    }
+
+    /**
+     * <p>PrintArray()</p>
+     * Overloaded function that is used to print to standard output
+     * Function parameters:
+     * @param intArray Reference to a two-dimensional array of integers
+     */
+    public static void printArray(int[][] intArray)
+    {
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = 0; j < intArray[i].length; j++) {
+                System.out.print(intArray[i][j] + " ");
+            }
+            System.out.print("\n");
+
+        }
+    }
+
+    /**
+     * <p>PrintArray()</p>
+     * Overloaded function that is used to print to standard output
+     * Function parameters:
+     * @param stringArray A reference to a one-dimensional array of strings
+     */
+    public static void printArray(String[] stringArray)
+    {
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.println(stringArray[i]);
+        }
+    }
+
+    /**
+     * <p>fill2DArray()</p>
+     * Function for filling a two-dimensional array of ints
+     * Function parameters:
+     * @param arr Reference to a two-dimensional array of ints
+     */
+    public static void fill2DArray(int[][] arr)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = rand.nextInt(4, 34);
+            }
+        }
+    }
+
+    /**
+     * <p>printMaxArrays()</p>
+     * Function for printing maxes of rows of 2d-array
+     * Function parameters:
+     * @param arr Reference to a two-dimensional array of ints
+     */
+    public static void printMaxArrays(int[][] arr) {
+        System.out.println("Array of max");
+        for (int i = 0; i < arr.length; i++) {
+            int max = arr[i][0];
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max)
+                    max = arr[i][j];
+            }
+            System.out.print(max + " ");
+        }
+        System.out.println();
+    }
+
+
+    public void start(int rows, int cols) {
+        System.out.println("Standart way");
+
+        int[][] intArr = new int[rows][cols];
+
+        System.out.println("2D Array");
+        fill2DArray(intArr);
+        printArray(intArr);
+        printMaxArrays(intArr);
+
+        System.out.println("Result");
+        String[] strArr = new String[rows];
+        fillStringArray(strArr, intArr);
+        sortStringArray(strArr);
+        printArray(strArr);
+    }
 }
